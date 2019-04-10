d27cb2b0774828abf6a2fbf051983fa22958ab60
soymanucho <31602774+soymanucho@users.noreply.github.com>
2019-04-10 12:06:16 -0300

    Update DiagnosisFactory.php

--- database/factories/DiagnosisFactory.php
+++ database/factories/DiagnosisFactory.php
@@ -2,24 +2,12 @@
 
 use App\Expedient;
 use App\DiagnosisType;
-use App\Patient;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Diagnosis;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
 
 $factory->define(Diagnosis::class, function (Faker $faker) {
     return [
-
         'diagnosisType_id' =>  DiagnosisType::inRandomOrder()->first(),
         'expedient_id'  =>  Expedient::inRandomOrder()->first(),
         'patient_id' => 0,//Patient::inRandomOrder()->first(), //copiar lo de arriba cuando tengamos la lista de pasientes
