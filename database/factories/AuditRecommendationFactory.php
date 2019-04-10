c50b8d079d047b6890c75b951cc81dd3292f75bd
soymanucho <31602774+soymanucho@users.noreply.github.com>
2019-04-10 11:15:39 -0300

    Update AuditRecommendationFactory.php

--- database/factories/AuditRecommendationFactory.php
+++ database/factories/AuditRecommendationFactory.php
@@ -7,6 +7,6 @@ use App\Audit;
 $factory->define(App\AuditRecommendation::class, function (Faker $faker) {
     return [
       'recommendation_id'=> Recommendation::orderByRaw('RAND()')->first(),
-      'audit_id'=> Audit::orderByRaw('RAND()')->first(),
+      'audit_id'=> Audit::orderByRaw('RANDOM()')->first(),
     ];
 });
