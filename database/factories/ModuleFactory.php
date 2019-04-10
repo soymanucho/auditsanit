03744a1e330417719166872c49c8248ec00a4953
soymanucho <31602774+soymanucho@users.noreply.github.com>
2019-04-10 11:33:15 -0300

    Update ModuleFactory.php

--- database/factories/ModuleFactory.php
+++ database/factories/ModuleFactory.php
@@ -7,8 +7,8 @@ use App\Module;
 
 $factory->define(Module::class, function (Faker $faker) {
     return [
-      'module_type_id' =>  ModuleType::orderByRaw('RAND()')->first(),
-      'module_category_id' =>  ModuleCategory::orderByRaw('RAND()')->first(),
+      'module_type_id' =>  ModuleType::inRandomOrder()->first(),
+      'module_category_id' =>  ModuleCategory::inRandomOrder()->first(),
       'price'=> $faker->randomNumber(4,false),
     ];
 });
