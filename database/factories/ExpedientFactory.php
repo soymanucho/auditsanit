338c7d48623b77d00ed106c5e49babedf3726743
soymanucho <31602774+soymanucho@users.noreply.github.com>
2019-04-10 11:19:14 -0300

    Update ExpedientFactory.php

--- database/factories/ExpedientFactory.php
+++ database/factories/ExpedientFactory.php
@@ -21,8 +21,8 @@ $factory->define(Expedient::class, function (Faker $faker) {
 
     return [
         'title' => $faker->sentence( 6, true),
-        'client_id' =>  Client::orderByRaw('RAND()')->first(),
-        'patient_id' =>  Patient::orderByRaw('RAND()')->first(),
+        'client_id' =>  Client::inRandomOrder()->first(),
+        'patient_id' =>  Patient::inRandomOrder()->first(),
       
     ];
 });
