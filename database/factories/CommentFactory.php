3c4aa8f35252728baff867d8d4936297be32e3c1
soymanucho <31602774+soymanucho@users.noreply.github.com>
2019-04-10 11:21:01 -0300

    Update CommentFactory.php

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -5,7 +5,7 @@ use App\Audit;
 use App\Comment;
 $factory->define(Comment::class, function (Faker $faker) {
     return [
-        'audit_id' =>  Audit::orderByRaw('RANDOM()')->first(),
+        'audit_id' =>  Audit::inRandomOrder()->first(),
         "text"=>$faker->sentence( 8, true),
     ];
 });
