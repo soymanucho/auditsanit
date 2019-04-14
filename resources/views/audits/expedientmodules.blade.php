4c63e08da2c9f4c160c767ab461609c2519f726b
Sebastian <sebastianbadaro@gmail.com>
2019-04-14 17:08:15 -0300

    fix view

--- resources/views/audits/expedientmodules.blade.php
+++ resources/views/audits/expedientmodules.blade.php
@@ -11,10 +11,11 @@
      @foreach ($audit->expedient->expedientModules as $expedientModule )
         <div class="tab-pane" id="modtab_{{$expedientModule->id}}">
           <h3>{{$expedientModule->module->moduleType->name}} - {{$expedientModule->module->moduleCategory->name}}
-            @if ($expedientModule->medicalServices->count()==0)
-                <small><strong> (Este Modulo no posee Prestaciones)</strong></small>
-            @endif
+
           </h3>
+          @if ($expedientModule->medicalServices->count()==0)
+              <small><strong> (Este Modulo no posee Prestaciones)</strong></small>
+          @endif
 
 
 
