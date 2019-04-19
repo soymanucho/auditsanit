f8a7262570fb391a3105037938fcb8d32017aa38
Manu <alvarrez.jmanuel94@gmail.com>
2019-04-19 20:14:44 -0300

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,6 @@
 
 @section('content')
 
-    <iframe width="100%" height="83%" src="https://app.powerbi.com/view?r=eyJrIjoiM2E3YjE5YzYtZGU5OC00YjFkLTk4MGMtOWYzMTUyNGIwMzQ1IiwidCI6IjdmYzk4YjgzLTU1NzUtNDA0Mi1iNDc2LWY5ODFhY2RiODY3MyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe width="100%" height="800px" src="https://app.powerbi.com/view?r=eyJrIjoiM2E3YjE5YzYtZGU5OC00YjFkLTk4MGMtOWYzMTUyNGIwMzQ1IiwidCI6IjdmYzk4YjgzLTU1NzUtNDA0Mi1iNDc2LWY5ODFhY2RiODY3MyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
 
 @endsection
