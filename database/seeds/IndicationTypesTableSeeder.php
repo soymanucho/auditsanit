1b2b442cedef47827bfbe61c0a799ce2f4faac06
SoyManucho <alvarez.jmanuel94@gmail.com>
2019-04-23 16:37:30 -0300

    seeders with real data

--- database/seeds/IndicationTypesTableSeeder.php
+++ database/seeds/IndicationTypesTableSeeder.php
@@ -11,11 +11,20 @@ class IndicationTypesTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('indication_types')->insert(['name' => "Indicacion 1"]);
-      DB::table('indication_types')->insert(['name' => "Indicacion 2"]);
-      DB::table('indication_types')->insert(['name' => "Indicacion 3"]);
-      DB::table('indication_types')->insert(['name' => "Indicacion 4"]);
-      DB::table('indication_types')->insert(['name' => "Indicacion 5"]);
+      DB::table('indication_types')->insert(['name' => "Psicología"]);
+      DB::table('indication_types')->insert(['name' => "Psicopedagogía"]);
+      DB::table('indication_types')->insert(['name' => "Fonoaudiología"]);
+      DB::table('indication_types')->insert(['name' => "Kinesiología"]);
+      DB::table('indication_types')->insert(['name' => "Terapia Ocupacional"]);
+      DB::table('indication_types')->insert(['name' => "Acompañante Terapéutico"]);
+      DB::table('indication_types')->insert(['name' => "Cuidador Domiciliario"]);
+      DB::table('indication_types')->insert(['name' => "Equinoterapia"]);
+      DB::table('indication_types')->insert(['name' => "Hidroterapia"]);
+      DB::table('indication_types')->insert(['name' => "Musicoterapia"]);
+      DB::table('indication_types')->insert(['name' => "Neurokinesiología"]);
+      DB::table('indication_types')->insert(['name' => "Pedagogía Especial"]);
+      DB::table('indication_types')->insert(['name' => "Psicomotricidad"]);
+      DB::table('indication_types')->insert(['name' => "Rehabilitación auditiva y del lenguaje"]);
 
     }
 }
