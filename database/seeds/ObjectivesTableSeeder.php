cdd825dd6acb1a8015308d0dd0458cf5a8a4fce0
SoyManucho <alvarez.jmanuel94@gmail.com>
2019-05-01 01:54:56 -0300

    Seeder objectives real

--- database/seeds/ObjectivesTableSeeder.php
+++ database/seeds/ObjectivesTableSeeder.php
@@ -11,6 +11,16 @@ class ObjectivesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Objective::class, 10)->create();
+      DB::table('objectives')->insert(['name' => "Verificar si recibe las prestaciones indicadas"]);
+      DB::table('objectives')->insert(['name' => "Comprobar si cumple el cronograma del expediente"]);
+      DB::table('objectives')->insert(['name' => "Comprobar si las prestaciones se realizan en domicilio declarado en expediente"]);
+      DB::table('objectives')->insert(['name' => "Descripción y análisis de antecedentes prestacionales"]);
+      DB::table('objectives')->insert(['name' => "Descripción y análisis de proyecto de trabajo actual"]);
+      DB::table('objectives')->insert(['name' => "Descripción y análisis de evolución por cada prestación"]);
+      DB::table('objectives')->insert(['name' => "Comprobar la necesidad de adicional por dependencia"]);
+      DB::table('objectives')->insert(['name' => "Comprobar la necesidad de las prestaciones indicadas"]);
+      DB::table('objectives')->insert(['name' => "Evaluar la pertinencia de las modalidades autorizadas"]);
+      DB::table('objectives')->insert(['name' => "Constatar correspondencia del kilometraje de transporte"]);
+      DB::table('objectives')->insert(['name' => "Ofrecer recomendaciones para mejorar la evolución del/la afiliado/a"]);
     }
 }
