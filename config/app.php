dba38756834e562f8c7b90f706a0faf42e53c5be
SoyManucho <alvarez.jmanuel94@gmail.com>
2019-05-23 21:48:48 -0300

    agregado el provider de la debugbar, deberias hacer composer install

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          Maatwebsite\Excel\ExcelServiceProvider::class,
          Laraveles\Spanish\SpanishServiceProvider::class,
          Spatie\Permission\PermissionServiceProvider::class,
+         Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
