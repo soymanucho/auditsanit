a0e1822e74e671ee315d492e6a4186dba2c660b3
Sebastian <sebastianbadaro@gmail.com>
2019-06-03 10:47:45 -0300

    fix print only one page

--- resources/views/audits/resume/auditDetailResume.blade.php
+++ resources/views/audits/resume/auditDetailResume.blade.php
@@ -57,139 +57,24 @@
 
 
 <!-- Main content -->
-<section class="invoice">
+<section class="invoice" style="page-break-before: auto">
   <!-- title row -->
-  <div class="row">
-    <div class="col-xs-12">
-      <h2 class="page-header">
-        <i class="fa fa-globe"></i> Auditoría sanitaria nº {{$audit->id}}
-        <small class="pull-right">Fecha: {{$audit->created_at}}</small>
-      </h2>
-    </div>
-    <!-- /.col -->
-  </div>
-  <!-- info row -->
-  <div class="row invoice-info">
-    <div class="col-sm-4 invoice-col">
-      <strong>Datos del afiliado</strong>
-      <address>
-        {{$audit->expedient->patient->person->surname}}, {{$audit->expedient->patient->person->name}}<br>
-        DNI {{$audit->expedient->patient->person->dni}}<br>
-        Dirección: {{$audit->expedient->patient->person->address->street}} {{$audit->expedient->patient->person->address->number}}, {{$audit->expedient->patient->person->address->location->name}} ({{$audit->expedient->patient->person->address->location->province->name}})<br>
-        Edad: {{$audit->expedient->patient->person->age()}}<br>
-        Género: @isset($audit->expedient->patient->person->gender)
-          {{$audit->expedient->patient->person->gender->name}}
-        @else
-          Indefinido
-        @endisset
-      </address>
-    </div>
-    <!-- /.col -->
-    <div class="col-sm-4 invoice-col">
-      <strong>Diagnósticos:</strong>
-      <address>
-        {{-- <strong>John Doe</strong><br> --}}
-        @foreach ($audit->expedient->diagnoses as $diagnosis)
-            {{$diagnosis->diagnosisType->name}}<br>
-        @endforeach
-      </address>
-    </div>
-    <!-- /.col -->
-    <div class="col-sm-4 invoice-col">
-      <strong>Indicaciones - Médico (Cant. sesiones):</strong>
-      <address>
-        {{-- <strong>John Doe</strong><br> --}}
-        @foreach ($audit->expedient->indications as $indication)
-            {{$indication->indicationType->name}} - {{$indication->medic->person->fullName()}} ({{$indication->numberOfSesions}} sesiones)<br>
-        @endforeach
-      </address>
-    </div>
-    <!-- /.col -->
-  </div>
-  <!-- /.row -->
 
-  <!-- Table row -->
-  <div class="row">
-    @foreach ($audit->expedient->expedientModules as $expedientModule )
-
-
-
-      <div class="col-xs-12 table-responsive">
-        <h4>{{$expedientModule->module->moduleType->name}} - {{$expedientModule->module->moduleCategory->name}}</h4>
-        <table class="table table-striped">
-          <thead>
-          <tr>
-            <th>Prestación</th>
-            <th>Prestador</th>
-            <th>Auditor</th>
-          </tr>
-          </thead>
-          <tbody>
-            @foreach ($expedientModule->medicalServices as $medicalService)
-              <tr>
-                <td>{{$medicalService->service->serviceType->name}}</td>
-                <td>{{$medicalService->service->vendor->name}} @isset($medicalService->service->vendor->address)
-                  ({{$medicalService->service->vendor->address->street}} {{$medicalService->service->vendor->address->number}})
-                @endisset </td>
-                <td>{{$medicalService->auditor->person->name}} {{$medicalService->auditor->person->surname}}</td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
-          @foreach ($expedientModule->medicalServices as $medicalService)
-        <h4>Informe del auditor {{$medicalService->auditor->person->name}} para {{$medicalService->service->serviceType->name}}</h4>
-        <div class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-          {!!$medicalService->report!!}
-        </div>
-          @endforeach
-      </div>
-
-    @endforeach
-    <!-- /.col -->
-  </div>
-  <!-- /.row -->
-
-  <div class="row">
-    <!-- accepted payments column -->
-    <div class="col-xs-12">
-      <p class="lead">Conclusión de la coordinadora:</p>
-      {{-- <img src="../../dist/img/credit/visa.png" alt="Visa">
-      <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-      <img src="../../dist/img/credit/american-express.png" alt="American Express">
-      <img src="../../dist/img/credit/paypal2.png" alt="Paypal"> --}}
-
-      <div class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-        {!!$audit->conclution!!}
-      </div>
-    </div>
-    <!-- /.col -->
-    <div class="col-xs-12">
-      <p class="lead">Recomendaciones de la coordinadora:</p>
-
-      <div class="table-responsive">
-        <table class="table">
-          @foreach ($audit->recommendations as $recommendation)
-            <tr>
-              <th> - {{$recommendation->name }}</th>
-            </tr>
-          @endforeach
-        </table>
-      </div>
-    </div>
-
-    <!-- /.col -->
-  </div>
-  <!-- /.row -->
+@include('audits.resume.resumeBody')
 
   <!-- this row will not appear when printing -->
   <div class="row no-print">
-    <div class="col-xs-12">
-      {{-- <a href="{!! route('audit-resume-print',compact('audit')) !!}" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Imprimir</a> --}}
-      {{-- <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment
-      </button>
-      <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
-        <i class="fa fa-download"></i> Generate PDF
+    <div class="col-xs-12 no-print">
+      <a href="{!! route('audit-resume-print',compact('audit')) !!}" target="_blank" class="btn btn-default pull-right"><i class="fa fa-print"></i> Imprimir</a>
+
+      {{-- <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
+        <i class="fa fa-download"></i> Generar PDF
       </button> --}}
+        </div>
+        <br>
+        <br>
+
+      <div class="col-xs-12 no-print">
       @can ('audit-edit-resume')
         <form action="{!! route('update-status-audit',['audit'=>$audit,'status'=>$audit->currentStatus()]) !!}" method="post">
               {{ csrf_field() }}
--- resources/views/audits/resume/auditDetailResumePrint.blade.php
+++ resources/views/audits/resume/auditDetailResumePrint.blade.php
@@ -25,152 +25,23 @@
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+
+<style>
+@media print{
+  *{
+    page-break-before: auto !important;
+    page-break-inside: auto !important;
+    overflow: visible !important;
+    
+  }
+  }
+</style>
 <body onload="window.print();">
 <div class="wrapper">
   <!-- Main content -->
   <section class="invoice">
-    <!-- title row -->
-    <div class="row">
-      <div class="col-xs-12">
-        <h2 class="page-header">
-          <i class="fa fa-globe"></i> Auditoría sanitaria nº {{$audit->id}}
-          <small class="pull-right">Fecha: {{$audit->created_at}}</small>
-        </h2>
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- info row -->
-    <div class="row invoice-info">
-      <div class="col-sm-4 invoice-col">
-        Datos del afiliado
-        <address>
-          <strong>{{$audit->expedient->patient->person->surname}}, {{$audit->expedient->patient->person->name}}</strong><br>
-          DNI {{$audit->expedient->patient->person->dni}}<br>
-          Dirección: {{$audit->expedient->patient->person->address->street}} {{$audit->expedient->patient->person->address->number}}, {{$audit->expedient->patient->person->address->location->name}} ({{$audit->expedient->patient->person->address->location->province->name}})<br>
-          Edad: {{$audit->expedient->patient->person->age()}}<br>
-          Género: @isset($audit->expedient->patient->person->gender)
-            {{$audit->expedient->patient->person->gender->name}}
-          @else
-            Indefinido
-          @endisset
-        </address>
-      </div>
-      <!-- /.col -->
-      <div class="col-sm-4 invoice-col">
-        <strong>Diagnósticos:</strong>
-        <address>
-          {{-- <strong>John Doe</strong><br> --}}
-          @foreach ($audit->expedient->diagnoses as $diagnosis)
-              {{$diagnosis->diagnosisType->name}}<br>
-          @endforeach
-        </address>
-      </div>
-      <!-- /.col -->
-      <div class="col-sm-4 invoice-col">
-        <strong>Indicaciones:</strong>
-        <address>
-          {{-- <strong>John Doe</strong><br> --}}
-          @foreach ($audit->expedient->indications as $indication)
-              {{$indication->indicationType->name}} ({{$indication->numberOfSesions}} sesiones)<br>
-          @endforeach
-        </address>
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
+  @include('audits.resume.resumeBody')
 
-    <!-- Table row -->
-    <div class="row">
-      @foreach ($audit->expedient->expedientModules as $expedientModule )
-        <div class="col-xs-12 table-responsive">
-          <h4>{{$expedientModule->module->moduleType->name}} - {{$expedientModule->module->moduleCategory->name}}</h4>
-          <table class="table table-striped">
-            <thead>
-            <tr>
-              <th>Prestación</th>
-              <th>Prestador</th>
-              <th>Auditor</th>
-            </tr>
-            </thead>
-            <tbody>
-              @foreach ($expedientModule->medicalServices as $medicalService)
-                <tr>
-                  <td>{{$medicalService->service->serviceType->name}}</td>
-                  <td>{{$medicalService->service->vendor->name}} @isset($medicalService->service->vendor->address)
-                    ({{$medicalService->service->vendor->address->street}} {{$medicalService->service->vendor->address->number}})
-                  @endisset </td>
-                  <td>{{$medicalService->auditor->person->name}} {{$medicalService->auditor->person->surname}}</td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
-          <h4>Informe del auditor</h4>
-          <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-            {{$audit->report}}
-          </p>
-        </div>
-      @endforeach
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
-
-    <div class="row">
-      <!-- accepted payments column -->
-      <div class="col-xs-6">
-        <p class="lead">Conclusión de la coordinadora:</p>
-        {{-- <img src="../../dist/img/credit/visa.png" alt="Visa">
-        <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-        <img src="../../dist/img/credit/american-express.png" alt="American Express">
-        <img src="../../dist/img/credit/paypal2.png" alt="Paypal"> --}}
-
-        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-          {{$audit->conclution}}
-        </p>
-      </div>
-      <!-- /.col -->
-      <div class="col-xs-6">
-        <p class="lead">Modulos recomendados</p>
-
-        <div class="table-responsive">
-          <table class="table">
-            <tr>
-              <th style="width:50%">Módulo:</th>
-              <th style="width:50%">Precio:</th>
-            </tr>
-            @foreach ($audit->expedient->expedientModules as $expedientModule )
-              <tr>
-                {{-- <td>{{$expedientModule->recommendedModule->moduleType->name}}</td>
-                <td>${{$expedientModule->recommendedModule->moduleType->price}}</td> --}}
-              </tr>
-            @endforeach
-
-          </table>
-        </div>
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
-
-    <!-- this row will not appear when printing -->
-    <div class="row no-print">
-      <div class="col-xs-12">
-        <a href="{!! route('audit-resume-print',compact('audit')) !!}" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
-        {{-- <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment
-        </button>
-        <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
-          <i class="fa fa-download"></i> Generate PDF
-        </button> --}}
-        @can ('audit-edit-resume')
-          <form action="{!! route('update-status-audit',['audit'=>$audit,'status'=>$audit->currentStatus()]) !!}" method="post">
-                {{ csrf_field() }}
-                @role('Administrador')
-                  <input type="submit" class="form-control btn btn-danger " @if ($audit->currentStatus()->id <= 4) style="display:none" @endif  @if ($audit->currentStatus()->id > 5) disabled  @endif name="updateStatus" value="Guardar y finalizar">
-                @endrole
-
-          </form>
-        @endcan
-      </div>
-    </div>
   </section>
   <!-- /.content -->
 
